3a8e9182e525f2952a60e91ed95613e0b25eddea
jomagalo <jomagalo@gmail.com>
2022-10-18 11:02:00 +0200

    Update constants.py

--- electrum/constants.py
+++ electrum/constants.py
@@ -102,9 +102,7 @@ class BitcoinMainnet(AbstractNet):
     BIP44_COIN_TYPE = 0
     LN_REALM_BYTE = 0
     LN_DNS_SEEDS = [
-        'nodes.lightning.directory.',
-        'lseed.bitcoinstats.com.',
-        'lseed.darosior.ninja',
+        'lseed.nefis.es',
     ]
 
 
