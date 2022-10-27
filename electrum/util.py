dcaeea0de0e1020f13e50f046ef257648cb862ab
jomagalo <jomagalo@gmail.com>
2022-10-27 15:06:28 +0200

    Update util.py

--- electrum/util.py
+++ electrum/util.py
@@ -826,6 +826,8 @@ def time_difference(distance_in_time, include_seconds):
         return "over %d years" % (round(distance_in_minutes / 525600))
 
 mainnet_block_explorers = {
+    'Jomagalo': ('http://explorer.nefis.es:3002/',
+						{'tx': 'transactions/', 'addr': 'addresses/'}),
     'Bitupper Explorer': ('https://bitupper.com/en/explorer/bitcoin/',
                         {'tx': 'transactions/', 'addr': 'addresses/'}),
     'Bitflyer.jp': ('https://chainflyer.bitflyer.jp/',
